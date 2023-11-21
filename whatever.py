2dd41024db02b38592fa55d93b2344cfee268b7b
ItsaPlant <krzysiekmichalik6@gmail.com>
2023-11-21 02:31:47 +0100

    add count dict

--- whatever.py
+++ whatever.py
@@ -7,8 +7,10 @@ for shop in shops:
     items_cap = [item.capitalize() for item in shops[shop]]
     print(f'in {shop.capitalize()} I need to buy: {items_cap}')
 print(f'overall there was {count} items')
+shopping_dict = {shop: len(shops[shop]) for shop in shops}
+print(shopping_dict)
 
-div_5 = [i for i in range(101) if i%5 == 0]
-div_5_3 = [ i**3 for i in div_5]
+div_5 = [i for i in range(101) if i % 5 == 0]
+div_5_3 = [i**3 for i in div_5]
 print(div_5)
 print(div_5_3)
