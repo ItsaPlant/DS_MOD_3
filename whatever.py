685def2cb2a9f1ef471f12813a9a1e724c18fd6b
ItsaPlant <83190454+ItsaPlant@users.noreply.github.com>
2023-12-02 15:04:35 +0100

    works, issue line 5

--- whatever.py
+++ whatever.py
@@ -2,13 +2,14 @@ shops = dict(shop1=['item_1', 'item_12'], shop2=['item_2', 'ite_22'], shop3=['it
 
 shop: str
 count = 0
-for shop in shops:
-    count += len(shops[shop])
-    items_cap = [item.capitalize() for item in shops[shop]]
-    print(f'in {shop.capitalize()} I need to buy: {items_cap}')
-print(f'overall there was {count} items')
+
 shopping_dict = {shop: len(shops[shop]) for shop in shops}
 print(shopping_dict)
 for shop in shopping_dict:
     print(f'I go to {shop} and buy {shopping_dict[shop]} items')
 
+div_5 = [i for i in range(101) if i % 5 == 0]
+div_5_3 = [i**3 for i in div_5]
+print(div_5)
+print(div_5_3)
+#blablabla
